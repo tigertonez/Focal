8d1043eb8bef5ee3333a8a9234500e22bf343e53
Anton Berndt <dernetteanton@icloud.com>
2025-07-29 14:30:16 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(main)/inputs/page.tsx
+++ src/app/(main)/inputs/page.tsx
@@ -14,6 +14,7 @@ import { Label } from '@/components/ui/label';
 import { Switch } from '@/components/ui/switch';
 import { SelectField } from '@/components/app/inputs/SelectField';
 import { SelectItem } from '@/components/ui/select';
+import { Input } from '@/components/ui/input';
 import {
   Collapsible,
   CollapsibleContent,
