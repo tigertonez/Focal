41cc589758980d3e11ae136246b7107487081f77
Anton Berndt <dernetteanton@icloud.com>
2025-07-30 20:52:20 +0000

    TRY THIS ONE INSTEAD: AIzaSyADmGMZdQDLQ68IP06ks1vQ9e53dOvDP88

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,4 +2,5 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})
+  plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})],
+});
